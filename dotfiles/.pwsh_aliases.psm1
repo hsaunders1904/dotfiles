fd0d619707d0d6938357c25e698de777b57f2ac9
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2022-09-19 00:38:00 +0100

    Add pwsh dotfiles

---
+++ dotfiles/.pwsh_aliases.psm1
@@ -0,0 +1,50 @@
+Set-Alias addpath add-path
+Set-Alias apath add-path
+Set-Alias bak New-Backup
+Set-Alias cdg Set-LocationGitRoot
+Set-Alias cg Set-LocationGitRoot
+Set-Alias cpath Copy-Path
+Set-Alias IM Import-Module
+Set-Alias IVS Import-VisualStudio
+Set-Alias notepad++ ${Env:ProgramFiles}\Notepad++\notepad++.exe
+Set-Alias npp ${Env:ProgramFiles}\Notepad++\notepad++.exe
+Set-Alias op Invoke-Item
+Set-Alias print Write-Output
+Set-Alias pycharm Invoke-PyCharm
+Set-Alias sha sha1
+Set-Alias shutdown-in Invoke-ShutdownIn
+Set-Alias shred sdelete
+Set-Alias touch ni
+Set-Alias tp Test-Path
+Set-Alias VisualStudio Invoke-VisualStudio
+Set-Alias vs Invoke-VisualStudio
+Set-Alias zip Compress-Archive
+
+# Typos
+## git
+$_GitTypos = @(
+    "giot",
+    "gitf",
+    "gitg",
+    "gitr",
+    "giut",
+    "giyt",
+    "goit",
+    "got",
+    "gt",
+    "gti",
+    "guit",
+    "gut",
+    "ggit"
+)
+foreach ($_Typo in ${_GitTypos}) { Set-Alias ${_Typo} git }
+
+## cd
+$_CdTypos = @("cds", "dc")
+foreach ($_Typo in ${_CdTypos}) { Set-Alias ${_Typo} cd }
+
+## ls
+$_LsTypos = @("kls", "ks")
+foreach ($_Typo in ${_LsTypos}) { Set-Alias ${_Typo} ls }
+
+Export-ModuleMember -Function * -Alias *
