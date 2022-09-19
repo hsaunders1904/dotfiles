5cf5dc0de48351625866d5346eb777965a09c6db
Harry Saunders <>
2022-09-19 15:06:18 +0100

    Fix Ubuntu ppa script

--- scripts/add_ubuntu_apt_ppas.sh
+++ scripts/add_ubuntu_apt_ppas.sh
@@ -6,11 +6,11 @@ set -o nounset # raise error using unused variables
 ubuntu_release="$(lsb_release -cs)"
 
 # Kitware (for CMake)
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6AF7F09730B3F0A4
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ ${ubuntu_release} main" |
     sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
 sudo apt update
-sudo rm /usr/share/keyrings/kitware-archive-keyring.gpg
 sudo apt install kitware-archive-keyring
 
 # GitHub CLI
