245b1130a671ec6167b4c681b68338a2f3fa18bb
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2022-10-21 00:05:15 +0100

    Pull submodule zsh plugins in installer

--- installer/zsh.py
+++ installer/zsh.py
@@ -5,6 +5,7 @@ from installer import lib
 
 __all__ = ["install"]
 
+CUSTOM_ZSH_DIR = os.path.join(lib.REPO_ROOT, "apps", "oh-my-zsh")
 SH_COMMENT_CHAR = "#"
 HOME_ZSH_PATH = os.path.join(os.path.expanduser("~"), ".zshrc")
 THIS_ZSH_PATH = os.path.join(lib.REPO_ROOT, "dotfiles", ".zshrc")
@@ -12,6 +13,7 @@ OHMYZSH_URL = "https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
 
 
 def install():
+    pull_plugins()
     update_zshrc()
     if not ohmyzsh_installed():
         install_ohmyzsh()
@@ -34,12 +36,7 @@ def ohmyzsh_installed():
 def install_ohmyzsh():
     installer_path = os.path.join(lib.REPO_ROOT, "external", "ohmyzsh_install.sh")
     lib.download_file(OHMYZSH_URL, installer_path, log=False)
-    install_cmd = [
-        "sh",
-        installer_path,
-        "--unattended",
-        "--keep-zshrc",
-    ]
+    install_cmd = ["sh", installer_path, "--unattended", "--keep-zshrc"]
     print(f"[+] {' '.join(install_cmd)}")
     result = subprocess.run(install_cmd, stdout=subprocess.PIPE)
     if result.returncode != 0:
@@ -48,3 +45,8 @@ def install_ohmyzsh():
 
 def build_file_import_str(file_path: str):
     return f'if [ -f "{file_path}" ]; then\n' f'    . "{file_path}"\n' f"fi"
+
+
+def pull_plugins():
+    cmd = ["git", "submodule", "update", "--init", CUSTOM_ZSH_DIR]
+    lib.run_command(cmd)
