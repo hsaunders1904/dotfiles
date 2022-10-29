846b50ffa8d617bdeac7a552d9710807b523b9b9
Harry Saunders <hsaunders1904@icloud.com>
2022-10-29 15:57:59 +0100

    Simplify pwsh install-modules script

--- installer/scripts/install_modules.ps1
+++ installer/scripts/install_modules.ps1
@@ -7,16 +7,6 @@ $MODULES = @(
     "pscx"
 )
 
-function Install-Requirement() {
-    param ([string] $Req)
-
-    if (Get-Module -ListAvailable -Name ${Req}) {
-        Write-Output "Module '${Req}' already installed."
-    } else {
-        Install-Module "${Req}" -Scope CurrentUser -AcceptLicense -AllowClobber
-    }
-}
-
 foreach ($Req in ${MODULES}) {
-    Install-Requirement "${Req}"
+    Install-Module "${Req}" -Scope CurrentUser -AcceptLicense -AllowClobber
 }
