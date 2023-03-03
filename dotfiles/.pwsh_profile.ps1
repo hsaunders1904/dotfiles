a09b9d5d4d8d35683bdf91f238e9b5ebcf354378
Harry Saunders <harry.saunders@ukaea.uk>
2023-03-03 07:43:35 +0000

    Use new pwsh theme 'agnoster-custom'

--- dotfiles/.pwsh_profile.ps1
+++ dotfiles/.pwsh_profile.ps1
@@ -7,7 +7,7 @@ Import-Module "${Env:DOTFILES_DIR}/dotfiles/.pwsh_functions.psm1"
 if (Get-Command oh-my-posh) {
     $Env:POSH_GIT_ENABLED = $true
     $ThemesDir = "${Env:DOTFILES_DIR}/apps/oh-my-posh/themes"
-    $ThemeName = "multiverse-neon-custom"
+    $ThemeName = "agnoster-custom"
     oh-my-posh init pwsh --config "${ThemesDir}\${ThemeName}.omp.json" `
         | Invoke-Expression
 } else {
