073e281a4df65d442d3df80ab68906102724d5d7
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2025-05-06 14:27:33 +0100

    Fix PowerShell profile

--- dotfiles/.pwsh_profile.ps1
+++ dotfiles/.pwsh_profile.ps1
@@ -5,7 +5,7 @@ $Env:VIRTUAL_ENV_DISABLE_PROMPT = 1  # oh-my-posh deals with venv display
 # Configure aliases and the prompt
 Import-Module "${Env:DOTFILES_DIR}/dotfiles/.pwsh_aliases.psm1"
 Import-Module "${Env:DOTFILES_DIR}/dotfiles/.pwsh_functions.psm1"
-if (Get-Command oh-my-posh) {
+if (Test-Command oh-my-posh) {
     $Env:POSH_GIT_ENABLED = $true
     $ThemesDir = "${Env:DOTFILES_DIR}/apps/oh-my-posh/themes"
     $ThemeName = "amro-custom"
@@ -21,7 +21,7 @@ Import-Module Terminal-Icons
 Import-Module PsFzf
 Import-Module posh-git
 if (Test-Command zoxide) {
-    Invoke-Expression (& { (zoxide init --hook pwd powershell | Out-String) })
+    Invoke-Expression (& { (zoxide init powershell | Out-String) })
 }
 
 # System path additions
