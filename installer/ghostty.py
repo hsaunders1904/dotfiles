fa88bc11fd1f03e06bdb0fe3cf3f6007ed02a59c
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2026-03-24 13:34:05 +0000

    Add ghostty config

---
+++ installer/ghostty.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from installer.base import Installer
+
+
+class GhosttyInstaller(Installer):
+    def install(self) -> bool:
+        ok = True
+        rel_path = Path(".config") / "ghostty" / "config.ghostty"
+        host_path = Path.home() / rel_path
+        dotfile_path = self.dotfiles_home() / rel_path
+        ok &= self.make_symlink(dotfile_path, host_path, force=True)
+        return ok
+
+    def should_install(self) -> bool:
+        return self.is_executable("ghostty")
