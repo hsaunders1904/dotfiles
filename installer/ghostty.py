9686079bff6c01a681873e93d363dffa72c89ece
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2026-06-05 10:06:02 +0100

    Fix Ghostty installer

--- installer/ghostty.py
+++ installer/ghostty.py
@@ -7,7 +7,7 @@ class GhosttyInstaller(Installer):
     def install(self) -> bool:
         ok = True
         rel_path = Path(".config") / "ghostty" / "config.ghostty"
-        host_path = Path.home() / rel_path
+        host_path = Path.home() / rel_path.parent / "config"
         dotfile_path = self.dotfiles_home() / rel_path
         ok &= self.make_symlink(dotfile_path, host_path, force=True)
         return ok
